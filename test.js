1b37ef2d1470c5f40002adac69687cf5a92748bc
Matt Hall <matthew349hall@hotmail.com>
2018-06-21 21:44:49 -0400

    new

--- test.js
+++ test.js
@@ -4,20 +4,38 @@ const unsure = require('./lib/unsure')
 let d = new unsure(__dirname)
 
 // Load files
-d.loadFile('data/pokedex.json')
-d.loadFile('data/meteorite-landings.json')
+d.loadFile('data/data01.json')
+// d.loadFile('data/meteorite-landings.json')
 
 // query pokedex
-d.setActiveFile('data/pokedex.json')
-console.log(d.Query('pokemon.next_evolution.name'))
+d.setActiveFile('data/data01.json')
 
 d.Time('delete', 1e6, () => {
-  let b = d.Edit('pokemon', {
+  let b = d.Delete(d.HOME, 'quiz.maths.q1.maths.q1')
+})
+d.Time('edit', 1e6, () => {
+  let b = d.Edit('quiz.maths.q1.question', {
     'hello': 'world',
     'number': 1e6
   })
 })
+d.Time('insert', 1e6, () => {
+  let b = d.Insert('quiz.maths.q1.question', {
+    'hello': 'world',
+    'number': 1e6
+  })
+})
+d.Time('query', 1e6, () => {
+  let b = d.Query('quiz.maths')
+})
 
 // query meteorite landings
-d.setActiveFile('data/meteorite-landings.json')
-console.log(d.Query('root.geolocation'))
+// d.setActiveFile('data/meteorite-landings.json')
+// console.log(d.Query('root.geolocation'))
+
+
+d.saveFile().then(data => {
+	console.log("Save Success!")
+}).catch(err => {
+	console.log(err)
+})
