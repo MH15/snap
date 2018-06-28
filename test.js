b33922f93326a6cc1c7b76ea8140412e5fdb1793
Matt Hall <matthew349hall@hotmail.com>
2018-06-27 20:14:02 -0400

    Add universal '.' query command to query entire object

--- test.js
+++ test.js
@@ -10,24 +10,25 @@ d.loadFile('data/data01.json')
 // query pokedex
 d.setActiveFile('data/data01.json')
 
-d.Time('delete', 1e6, () => {
-  let b = d.Delete(d.HOME, 'quiz.maths.q1.maths.q1')
-})
-d.Time('edit', 1e6, () => {
-  let b = d.Edit('quiz.maths.q1.question', {
-    'hello': 'world',
-    'number': 1e6
-  })
-})
-d.Time('insert', 1e6, () => {
-  let b = d.Insert('quiz.maths.q1.question', {
-    'hello': 'world',
-    'number': 1e6
-  })
-})
-d.Time('query', 1e6, () => {
-  let b = d.Query('quiz.maths')
-})
+// d.Time('delete', 1e6, () => {
+//   let b = d.Delete(d.HOME, 'quiz.maths.q1.maths.q1')
+// })
+// d.Time('edit', 1e6, () => {
+//   let b = d.Edit('quiz.maths.q1.question', {
+//     'hello': 'world',
+//     'number': 1e6
+//   })
+// })
+// d.Time('insert', 1e6, () => {
+//   let b = d.Insert('quiz.maths.q1.question', {
+//     'hello': 'world',
+//     'number': 1e6
+//   })
+// })
+// d.Time('query', 1e6, () => {
+  let b = d.Query('.')
+  console.log(b)
+// })
 
 // query meteorite landings
 // d.setActiveFile('data/meteorite-landings.json')
